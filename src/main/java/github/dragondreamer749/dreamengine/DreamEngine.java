aa07fcef21bcefa7ca8f7ed1b6e8f7230ad0bdcd
KyleAdomeit <adomeitk@sheridancollege.ca>
2020-07-10 18:08:42 -0400

    added basic wip stuff to sequence and added main to DreamEngine.java

--- src/main/java/github/dragondreamer749/dreamengine/DreamEngine.java
+++ src/main/java/github/dragondreamer749/dreamengine/DreamEngine.java
@@ -1,14 +1,36 @@
 package github.dragondreamer749.dreamengine;
 
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
-public final class DreamEngine{
+public final class DreamEngine extends JPanel{
 	
 	
 	
-	public DreamEngine() {
+	public DreamEngine()  {
 		
+			
+			
+		}
+	
+	
+	public static void main(String args[]) {
 		
+	
+		JFrame frame = new JFrame();
+		frame.getContentPane().add(new DreamEngine());
+
+	      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	      frame.setSize(1200, 600);
+	      frame.setLocationRelativeTo(null);
+	      frame.setVisible(true);
 	}
 	
 	
--- src/main/java/github/dragondreamer749/dreamengine/sprites/Sequences.java
+++ src/main/java/github/dragondreamer749/dreamengine/sprites/Sequences.java
@@ -1,5 +1,60 @@
 package github.dragondreamer749.dreamengine.sprites;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import github.dragondreamer749.dreamengine.resources.ResourceLocation;
+import javax.imageio.ImageIO;
+
+
 public class Sequences {
 
+	
+	private HashMap <String, ArrayList<BufferedImage>> sequence = new HashMap <String, ArrayList<BufferedImage>>();
+	
+	private ArrayList<ResourceLocation> locations = new ArrayList<ResourceLocation>();
+	
+	public Sequences(HashMap <String, ArrayList<BufferedImage>> sequence) {
+		this.sequence = sequence;
+	}
+	
+	public boolean setSequence(String sequenceName, ArrayList<BufferedImage> sequence, ArrayList<ResourceLocation> locations) {
+		
+		if(!sequence.isEmpty() 
+				&& !sequenceName.isEmpty() && this.sequence.containsKey(sequenceName)
+				&& !locations.isEmpty()) {
+			
+			try {
+				
+				this.sequence.put(sequenceName, sequence); 
+				
+				
+			}catch(Exception e) {
+				return false;
+			}
+			
+			
+			
+			return true;
+		}
+		
+		return false;
+	}
+	
+	public void DrawImage() {
+		
+		BufferedImage img;
+		try {
+			img = ImageIO.read(new File(".png"));
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+	}
+	
+	
+	
 }
